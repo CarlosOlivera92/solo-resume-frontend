89055c6c43ec84840e9b8c20e7e308249e35bc8a
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-03-19 14:39:38 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import '@assets/styles/global.css';
+function App() {
+
+  return (
+    <h1>Holaaaa</h1>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@data": "/src/data",
+      "@hooks": "/src/hooks",
+      "@pages": "/src/pages",
+      "@templates": "/src/templates",
+      "@utils": "/src/utils",
+      "@": "/src",
+    },
+  },
+})
